751fba8de63436192e41d7e8d68ec2d2ed8142e2
whdrnal <whdrnal34@gmail.com>
2022-02-22 22:47:04 +0900

    아이디찾기, 비밀번호찾기 footer 추가 및 코드 정렬

--- config/settings/common.py
+++ config/settings/common.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'movie',  # DB명
         'USER': 'root',  # DBMS 접속 아이디
-        'PASSWORD': '',  # DBMS 접속 비번
+        'PASSWORD': '1234',  # DBMS 접속 비번
         'HOST': '127.0.0.1',  # DBMS 주소
         'PORT': '3306',  # DBMS 포트
         'OPTIONS': {
